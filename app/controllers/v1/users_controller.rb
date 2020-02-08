4420c6486862b7a772d285e54447ce2411534d07
KenAra <louis.9814@gmail.com>
2020-02-08 17:04:53 +0900

    Userのストロングパラメータを修正

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -21,6 +21,6 @@ class V1::UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :uid)
+      params.require(:user).permit(:name, :email, :uid, :admin, :profile_image)
     end
 end
