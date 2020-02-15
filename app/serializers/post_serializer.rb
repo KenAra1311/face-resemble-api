8793d2b6d38defeb0a13898574cd8afba8d4c541
KenAra <louis.9814@gmail.com>
2020-02-15 17:15:58 +0900

    JSON で返す投稿時間を整形

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,10 +1,14 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :image, :file_name, :emotion, :count, :user_id, :username, :created_at
+  attributes :id, :title, :content, :image, :file_name, :emotion, :count, :user_id, :username, :created
 
   belongs_to :user
   has_many :likes
 
   def username
     object.user.name
+  end
+
+  def created
+    object.created_at.strftime('%Y/%m/%d %H:%M')
   end
 end
