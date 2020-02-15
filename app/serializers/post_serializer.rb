56a63abb30bc409d0ac455c08045ab4696eae62b
KenAra <louis.9814@gmail.com>
2020-02-15 15:11:47 +0900

    Rails を日本時間に設定

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :image, :file_name, :emotion, :count, :user_id, :username
+  attributes :id, :title, :content, :image, :file_name, :emotion, :count, :user_id, :username, :created_at
 
   belongs_to :user
   has_many :likes
