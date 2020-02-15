8bea9a68400236d344c599095eda75ab82f1da2d
KenAra <louis.9814@gmail.com>
2020-02-15 17:41:42 +0900

    いいねをもらった総数を JSON で返す

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,17 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :admin, :profile_image, :file_name
-  
+  attributes :id, :name, :email, :admin, :profile_image, :file_name, :like_total_count
+
   has_many :posts
   has_many :likes
+
+  def like_total_count
+    i = 0
+    object.posts.each do |post|
+      if post.count != nil
+        i += post.count
+      end
+    end
+
+    return i
+  end
 end
