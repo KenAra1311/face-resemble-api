6c5d6ed5650fa6d9fdba5efdd982d48a0be7b2ef
KenAra <louis.9814@gmail.com>
2020-02-18 16:24:16 +0900

    binstubを作成

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,13 @@ module FaceResembleApi
 
     config.time_zone = 'Tokyo'
 
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
