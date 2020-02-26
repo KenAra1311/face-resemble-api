243694d70798c7c62b393346e8a4b378bb53480e
KenAra <louis.9814@gmail.com>
2020-02-26 09:58:10 +0900

    Like が生成された日時を JSON で出力

--- app/serializers/like_serializer.rb
+++ app/serializers/like_serializer.rb
@@ -1,6 +1,10 @@
 class LikeSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :post_id
+  attributes :id, :user_id, :post_id, :created
 
   belongs_to :user
   belongs_to :post
+
+  def created
+    object.created_at.strftime('%Y/%m/%d %H:%M')
+  end
 end
